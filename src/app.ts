a6ba2df86213c7240955979751a277936faa607f
scontr4 <scontr4@bancodebogota.com.co>
2024-11-21 15:13:35 -0500

    commented all code in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import { getPokemonById } from "./js-foundation/06-promises";
-import { buildLogger } from "./plugins/logger.plugin";
+// import { getPokemonById } from "./js-foundation/06-promises";
+// import { buildLogger } from "./plugins/logger.plugin";
 // const { getAge, getUUID } = require('./plugins');
 
 
@@ -13,10 +13,10 @@ import { buildLogger } from "./plugins/logger.plugin";
 // const getPokemonById = require('./js-foundation/06-promises');
 
  
-const logger = buildLogger('app.js');
+// const logger = buildLogger('app.js');
 
-logger.log('Hola mundo');
-logger.error('Esto es algo malo');
+// logger.log('Hola mundo');
+// logger.error('Esto es algo malo');
 
 // getPokemonById(4)
 //   .then( ( pokemon ) => console.log({ pokemon }) )
