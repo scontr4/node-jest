47221b5659205d979894042404924c8c41260e9f
scontr4 <scontr4@bancodebogota.com.co>
2024-11-22 00:15:17 -0500

    03 -> callback.ts FULL

--- tests/js-foundation/03-callbacks.test.ts
+++ tests/js-foundation/03-callbacks.test.ts
@@ -5,15 +5,35 @@ describe('js-foundation/03-callbacks.test.ts', () => {
   test('should return error is user doesnt exist', (done) => {  // done here
     
     const id = 10;
-    getUserById(10, (err, user) => {
+    getUserById(id, (err, user) => {
 
       expect(err).toBe(`User not found with id ${id}`);
       expect(user).toBeUndefined();
 
       done();  // done is called to end the test
-      
     })
     
+  })
+
+  test('should return correct user for id', () => { 
+    const id = 1;
+    getUserById(id, (err, user) => {
+      expect(err).toBeUndefined();
+
+      // ** To Be doesn't work with objects
+      // expect(user).toBe({
+      //   id: 1,
+      //   name: 'John Doe',
+      // })
+      expect(user).toStrictEqual({
+        id: 1,
+        name: 'John Doe',
+      })
+
+      // ** Evaluating properties using toBe
+      // expect(user!.id).toBe(1);
+      // expect(user!.name).toBe('John Doe');
+    })
   })
 
 })
