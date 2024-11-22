b74b84ad5a84b0b75b3a35f2c444933c56c230f8
scontr4 <scontr4@bancodebogota.com.co>
2024-11-22 01:18:20 -0500

    06 -> factory.ts

--- src/js-foundation/05-factory.ts
+++ src/js-foundation/05-factory.ts
@@ -1,7 +1,4 @@
 
-// const { getUUID } = require('../plugins/get-id.plugin');
-// const { getAge } = require('../plugins/get-age.plugin');
-// const { getAge, getUUID } = require('../plugins');
 interface BuildMakerPersonOptions {
   getUUID: () => string;
   getAge: (birthdate: string) => number;
@@ -26,13 +23,3 @@ export const buildMakePerson = ({ getAge, getUUID }: BuildMakerPersonOptions) =>
   }
 
 }
-
-
-// const obj = { name: 'John', birthdate: '1985-10-21' };
-
-// const john = buildPerson( obj );
-
-
-// console.log(john);
-
-
---
+++ tests/js-foundation/05-factory.test.ts
@@ -0,0 +1,27 @@
+import { buildMakePerson } from '../../src/js-foundation/05-factory';
+
+
+describe('05-factory.test.ts', () => { 
+
+  // Manual mocks
+  const getUUID = () => '1234'
+  const getAge = () => 40
+  
+  // Easiest test -> the typeof the return
+  test('buildMakePerson should return a function', () => { 
+    const makePerson = buildMakePerson({getAge, getUUID});
+    // expect(typeof(makePerson)).toBe('string'); // We can check comparing with what the console says
+    expect(typeof(makePerson)).toBe('function');
+  })
+
+  test('buildMakePerson should return a person Builder', () => { 
+    const makePerson = buildMakePerson({getAge, getUUID});  
+    const johnnyTest = makePerson({name: 'Johnny Test', birthdate: '1994-11-22'})
+    // console.log(johnnyTest)
+    expect(johnnyTest).toEqual(
+      { id: '1234', name: 'Johnny Test', birthdate: '1994-11-22', age: 40 }
+    )
+    // Why aren't we testing if the age is correct?...
+  })
+
+})
